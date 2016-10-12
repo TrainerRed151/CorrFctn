7b1ff69e4637302e460081a50f96ca2ef1ce9919
Brian Pomerantz <bbp114@psu.edu>
2016-10-12 15:15:23 -0400

    working first attempt

--- gr.c
+++ gr.c
@@ -5,46 +5,65 @@
 #include <math.h>
 
 int inDist(double x, double y, double r, double dr);
+double ran();
 
 int main(int argc, const char *argv[]) {
-    int np = 10;
+    int np = 1000;
     double rmax = sqrt(2.0);
     double dr = 0.01;
     //int rsize = int(rmax/dr) + 1;
     int rsize = 100;
+    FILE *opf = fopen("gr.dat", "w");
+    if (opf == NULL) {
+        printf("Can't open file gr.dat");
+        return 2;
+    }
 
     double r[rsize], x[np], y[np];
+    int gr[rsize];
 
-    int i, j;
-    for (i = 1; i < rsize; i++) {
+    int i, j, k;
+    for (i = 0; i < rsize; i++) {
         r[i] = (rmax/rsize)*i;
+        gr[i] = 0;
     }
     
     for (i = 0; i < np; i++) {
-        if (rand() < 0.5) {
-            x[i] = rand();
+        if (ran() < 0.5) {
+            x[i] = ran();
         }
         else {
-            x[i] = -rand();
+            x[i] = -ran();
         }
 
-        if (rand() < 0.5) {
-            y[i] = rand();
+        if (ran() < 0.5) {
+            y[i] = ran();
         }
         else {
-            y[i] = -rand();
+            y[i] = -ran();
         }
     }
 
     for (i = 0; i < rsize; i++) {
+        for (j = 0; j < np; j++) {
+            for (k = 0; k < np; k++) {
+                gr[i] += inDist(x[j], y[k], r[i], dr);
+            }
+        }
         
+        fprintf(opf, "%f, %d\n", r[i], gr[i]);
     }
 
+    fclose(opf);
     
     return 0;
 }
 
 int inDist(double x, double y, double r, double dr) {
     double dist = sqrt(x*x + y*y);
-    return dist > r - dr/2 && dist < r + dr/2;
+    return (dist > (r - dr/2)) && (dist < (r + dr/2));
+}
+
+double ran() {
+    return rand()/((double) RAND_MAX);
 }
